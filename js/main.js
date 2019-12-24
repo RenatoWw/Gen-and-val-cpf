8ba1a20b2ffd689819954e439f9e626e7c5be8f5
RenatoWw <renatoflud@protonmail.com>
2019-12-24 18:10:54 +0000

    front-end cpf validation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('beep boop');
+const val = () => {
+    let cpf = document.querySelector('#validar').value;
+    let result = false;
+    /* Beginning validation */
+    const calculationVerification = (cpf) => {
+        return ((cpf[0] * 10 + cpf[1] * 9 + cpf[2] * 8 + cpf[3] * 7 + cpf[4] * 6 + cpf[5] * 5 + cpf[6] * 4 + cpf[7] * 3 + cpf[8] * 2) * 10 % 11 == cpf[9]) ? ((cpf[0] * 11 + cpf[1] * 10 + cpf[2] * 9 + cpf[3] * 8 + cpf[4] * 7 + cpf[5] * 6 + cpf[6] * 5 + cpf[7] * 4 + cpf[8] * 3 + cpf[9] * 2) * 10 % 11 == cpf[10] ? true : false) : false;
+    }
+    const isANumber = (cpf) => !isNaN(cpf);
+    const hasLength = (cpf) => cpf.length == 11 ? true : false;
+    const isEqual = (array) => {
+        firstElement = array[0];
+        for(let i = 1; i < array.length; i++){
+            if(array[i] != firstElement){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    return isANumber(cpf) && hasLength(cpf) && isEqual(cpf) && calculationVerification(cpf) == true ? 'CPF válido' : 'CPF inválido';
+}
+
